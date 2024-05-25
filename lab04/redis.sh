fd171af5717b224f5a4e172fb4cae1a9177a9ea9
octolera <octoechoes@gmail.com>
2024-05-25 14:13:45 +0000

    l4

---
+++ lab04/redis.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cp nginx.conf /etc/nginx/nginx.conf
+mkdir /usr/share/nginx/l4
+cp -r ./music /usr/share/nginx/l4/
+cp -r ./pages /usr/share/nginx/l4/
+cp ./httpd.conf /etc/httpd/conf/httpd.conf
