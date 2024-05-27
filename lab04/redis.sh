58a927c313af8ef4dd97b59011c1cca72ca256b7
octolera <octoechoes@gmail.com>
2024-05-27 07:21:36 +0000

    l4

--- lab04/redis.sh
+++ lab04/redis.sh
@@ -3,4 +3,4 @@ cp nginx.conf /etc/nginx/nginx.conf
 mkdir /usr/share/nginx/l4
 cp -r ./music /usr/share/nginx/l4/
 cp -r ./pages /usr/share/nginx/l4/
-cp ./httpd.conf /etc/httpd/conf/httpd.conf
+#cp ./httpd.conf /etc/httpd/conf/httpd.conf
