322426c4a56810c59f65c8af7569c8e30c21cc2a
octolera <octoechoes@gmail.com>
2024-05-28 09:13:16 +0000

    l5

---
+++ lab05/redis.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cp nginx.conf /etc/nginx/nginx.conf
+mkdir /usr/share/nginx/l4
+cp -r music /usr/share/nginx/l4/
+cp -r pages /usr/share/nginx/l4/
+cp -r image1  /usr/share/nginx/l4/
+cp -r image2 /usr/share/nginx/l4/
+#cp ./httpd.conf /etc/httpd/conf/httpd.conf
