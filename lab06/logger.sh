f65eb3eb177ae015b7783d5aada92f24f30c6343
octolera <octoechoes@gmail.com>
2024-05-29 11:45:04 +0000

    l6

--- lab06/logger.sh
+++ lab06/logger.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 size=$(stat -c %s file2)
-logfile=.logs
+logfile=/usr/share/nginx/logs/common.log
 touch file{1..4}
 touch buf
 while :
 do 
     size=$(stat -c %s file1)
-    if [ $size -gt 307200 ]; then
+    if [ $size -gt 30720 ]; then
         lines=$(cat file1 | wc -l);
         current_date_time="`date +%Y-%m-%d_%H:%M:%S`";
         echo  "Log : $current_date_time lines: $lines" >> file2
         echo > file1
     fi
-    log=$(cat $logfile)
-    echo "$log" >> file1 && echo > logfile;
-    sed '/[4][0-9][0-9]/!d' file1 >> file3
-    sed  '/[5][0-9][0-9]/d' file1 >> buf && mv buf file1 && echo > buf;
-    awk '/[5][0-9][0-9]/' file1 >> file4;
-    sed  '/[5][0-9][0-9]/d' file1 >> buf && mv buf file1 && echo > buf;
-    echo "cycle"
+    log=$(tail -n20 $logfile)
+    #echo "$log" >> file1;
+   echo "$log" > buf
+     sed '/[4][0-9][0-9]/!d' buf >> file3;
+    sed  '/[5][0-9][0-9]/d' buf  >>  file1;
+    awk '/[5][0-9][0-9]/' buf >> file4;
+    sed  '/[5][0-9][0-9]/d' buf >> file1 && echo > buf;
     sleep 5
 
 done
